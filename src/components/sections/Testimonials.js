cefd11f58e11215a9be5261b38f840b3e0f73696
mihailo1995 <mihailo.ljubenkovic@gmail.com>
2019-11-07 10:13:24 +0100

    slider working on clicked dots

--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -1,77 +1,87 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { css } from "emotion";
+import { data } from "../../data/testimonialData";
 import Client from "./utils/Client";
 
-class Testimonials extends Component {
+function Testimonials() {
+    const [current, setCurrent] = useState(data[1]);
+    const [active, setActive] = useState(1);
 
-    render() {
-        return (
-            <section className="testimonials">
-                <h2 className="h2-universal">Kind words from our clients</h2>
-                <div className="carousel">
-                    <div className="slider">
-                        <Client 
-                            text="Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Necessitatibus amet aliquam
-                            pariatur explicabo quis ipsam ducimus porro
-                            doloremque similique."
-                            name="Bob Wick"
-                            title="CEO"
-                            company="Microsoft"
-                        />
-                        <Client 
-                            text="Lorem ipsum dolor sit amet, consectetuer
-                            adipisicing elit, sed diam nonummy nibh euismod
-                            tincidunt ut laoreet dolore magna aliquam."
-                            name="Yuri Markov"
-                            title="Marketing Director"
-                            company="FoFor"
-                        />
-                        <Client 
-                            text="Lorem ipsum dolor sit amet consectetur,
-                            adipisicing elit. Tenetur omnis minus sequi! A
-                            iste voluptas necessitatibus iusto doloremque
-                            ipsam unde."
-                            name="John Smith"
-                            title="Android developer"
-                            company="Google"
-                        />
-                    </div>
-                    <div className="control">
-                        <svg
-                            className="arrow left"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 28.36 53.89"
-                        >
-                            <title>Previous</title>
-                            <polygon
-                                className="cls-1"
-                                points="1.41 53.89 0 52.48 25.53 26.95 0 1.41 1.41 0 28.36 26.95 1.41 53.89"
-                            />
-                        </svg>
+    const onLiClick = (e) => {
+        setCurrent(data[e.target.getAttribute("data-li")]);
+        setActive(e.target.getAttribute("data-li"));
+    };
 
-                        <svg
-                            className="arrow right"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 28.36 53.89"
-                        >
-                            <title>Next</title>
-                            <polygon
-                                className="cls-1"
-                                points="1.41 53.89 0 52.48 25.53 26.95 0 1.41 1.41 0 28.36 26.95 1.41 53.89"
-                            />
-                        </svg>
-
-                        <ul>
-                            <li></li>
-                            <li className="selected"></li>
-                            <li></li>
-                        </ul>
-                    </div>
-                </div>
-                <a href="#!">View all clients</a>
-            </section>
-        );
+    const onLeftArrowClick = () => {
+        // setCurrent(data[current-1]);
+        // setActive(current-1);
+        console.log("Left");
     }
+
+    const onRightArrowClick = () => {
+        // setCurrent(data[current+1]);
+        // setActive(data[current+1]);
+        console.log("Right");
+    }
+
+    return (
+        <section className="testimonials">
+            <h2 className="h2-universal">Kind words from our clients</h2>
+            <div className="carousel">
+                <div className="slider">
+                    <Client
+                        text={current.text}
+                        name={current.name}
+                        title={current.title}
+                        company={current.company}
+                    />
+                </div>
+                <div className="control">
+                    <svg
+                        className="arrow left"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 28.36 53.89"
+                        onClick={onLeftArrowClick}
+                    >
+                        <title>Previous</title>
+                        <polygon
+                            className="cls-1"
+                            points="1.41 53.89 0 52.48 25.53 26.95 0 1.41 1.41 0 28.36 26.95 1.41 53.89"
+                        />
+                    </svg>
+
+                    <svg
+                        className="arrow right"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 28.36 53.89"
+                        onClick={onRightArrowClick}
+                    >
+                        <title>Next</title>
+                        <polygon
+                            className="cls-1"
+                            points="1.41 53.89 0 52.48 25.53 26.95 0 1.41 1.41 0 28.36 26.95 1.41 53.89"
+                        />
+                    </svg>
+
+                    <ul className={css`
+                        li[data-li="${active}"] {
+                            background-color: #144077 !important;
+                            transform: scale(1.5);
+                        }                    
+                    `}>
+                        {Object.keys(data).map((index) => (
+                            <li
+                                onClick={(e) => onLiClick(e)}
+                                data-li={index}
+                                key={index}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <a href="#!">View all clients</a>
+        </section>
+    );
 }
 
 export default Testimonials;
